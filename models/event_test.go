575aa1a7a9aed57b73b117362c8c71faee218b7a
Ildar Usmanov <wajox@mail.ru>
2017-09-11 14:35:03 +0300

    add tests

--- models/event_test.go
+++ models/event_test.go
@@ -1,10 +1,43 @@
 package models
 
 import (
+	"eventmapper/mq"
+	"errors"
 	"gopkg.in/validator.v2"
+	"encoding/json"
 	"testing"
 	"time"
+	"github.com/streadway/amqp"
 )
+
+type eventChannelMock struct {
+	result error
+}
+
+func (ch *eventChannelMock) PublishEvent(event mq.Event, rKey string) error {
+	return ch.result
+}
+
+func (ch *eventChannelMock) ConsumeEvents(rKeys string) (<-chan amqp.Delivery, error) {
+	deliveries := make(chan amqp.Delivery)
+
+	return (<-chan amqp.Delivery)(deliveries), nil
+}
+
+func (ch *eventChannelMock) Close() {
+	return
+}
+
+func createValidEvent() mq.Event {
+	validEvent := CreateNewEvent()
+	validEvent.EventName = "authorized"
+	validEvent.EventTarget = "user"
+	validEvent.UserId = "some-user-id"
+	validEvent.CreatedAt = time.Now().Unix()
+	validEvent.Params = map[string]string{"key1": "value1"}
+
+	return validEvent
+}
 
 func TestInvalidEventValidators(t *testing.T) {
 	invalidEvent := CreateNewEvent()
@@ -15,12 +48,7 @@ func TestInvalidEventValidators(t *testing.T) {
 }
 
 func TestValidEventValidators(t *testing.T) {
-	validEvent := CreateNewEvent()
-	validEvent.EventName = "authorized"
-	validEvent.EventTarget = "user"
-	validEvent.UserId = "some-user-id"
-	validEvent.CreatedAt = time.Now().Unix()
-	validEvent.Params = map[string]string{"key1": "value1"}
+	validEvent := createValidEvent()
 
 	if err := validator.Validate(validEvent); err != nil {
 		t.Error("Valid Event model validation: Nil expected, but", err, "given")
@@ -33,5 +61,38 @@ func TestGetEventName(t *testing.T) {
 
 	if e.GetEventName() != e.EventName {
 		t.Error(e.EventName, "expected, but", e.GetEventName(), "given")
+	}
+}
+
+func TestGetEventBody(t *testing.T) {
+	eSource := CreateNewEvent()
+	eResult := CreateNewEvent()
+
+	eSource.EventName = "authorized"
+	jsonBody, err := eSource.GetBody()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = json.Unmarshal(jsonBody, eResult)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if eResult.EventName != eSource.EventName {
+		t.Error("Encoded and decoded items are not equal")
+	}
+}
+
+func TestPublishEvent(t *testing.T) {
+	e := createValidEvent()
+	result := errors.New("mock")
+	ch := &eventChannelMock{result}
+	err := e.Publish(ch, "test")
+
+	if err != result {
+		t.Error("Unexpected result")
 	}
 }
--- mq/channel.go
+++ mq/channel.go
@@ -14,6 +14,7 @@ const (
 type Event interface {
 	GetEventName() string
 	GetBody() ([]byte, error)
+	Publish(EventChannel, string) error
 }
 
 type EventChannel interface {
